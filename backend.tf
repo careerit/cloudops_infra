d403bd29d517a584baa8c277845aa4dec658d291
kiran <kiranops@yahoo.com>
2021-05-24 21:22:19 +0530

    backend config added

--- backend.tf
+++ backend.tf
@@ -3,6 +3,6 @@ terraform {
     resource_group_name  = "InfraBase"
     storage_account_name = "tfsxyzoi1"
     container_name       = "terraformbackend"
-    key                  = "azdotf"
+    key                  = "cloudopstf"
   }
 }
