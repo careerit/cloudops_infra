1f0e61b1f8a2c09ad531e008726e0e19353fab4f
kiran <kiranops@yahoo.com>
2021-05-24 22:16:30 +0530

    checking secret file

--- prebuild.sh
+++ prebuild.sh
@@ -1,5 +1,9 @@
 ls -l
+cat $(id_demo.secureFilePath)
+cat $(id_demo.secureFilePath) > id_demo
 
+ls -l
+chmod 400 id_demo
 sed  "s,#PREFIX#,$PREFIX," terraform.tfvars.tpl > terraform.tfvars
 sed -i "s,#LOCATION#,$LOCATION," terraform.tfvars
 sed -i "s,#VMPASSWORD#,$VMPASSWORD," terraform.tfvars
